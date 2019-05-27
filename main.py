a95c7b6c06738b80d6eea9d7158439a7e2a1ba50
Sonkaryasshu <sonkaryasshu@gmail.com>
2019-05-27 15:18:30 +0530

    working

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pyttsx3
 import datetime
 import speech_recognition as sr
+import wikipedia
+import os
+import webbrowser
 
-engine = pyttsx3.init('espeak')
+engine = pyttsx3.init('sapi5')
 engine.setProperty('voice', 'english-us')
 
 ''' VOLUME '''
 voulume = engine.getProperty('volume')
-print(voulume)
+# print(voulume)
 
 ''' RATE '''
 rate = engine.getProperty('rate')   # getting details of current speaking rate
-print (rate)                        #printing current voice rate
+# print (rate)                        #printing current voice rate
 
 
 def speak(audio):
@@ -37,14 +40,34 @@ def takeCommand():
     
     try:
         print("Recognizing...")
-        query = r.recognize_sphinx(audio, language='en-in')
+        query = r.recognize_google(audio, language='en-in')
         print(f"User : {query}\n")
     except Exception as err:
         print(err)
-        print("Please say that again")
+        print("Sorry sir! Please say that again")
+        speak("Sorry sir! Please say that again")
         return "None"
     return query
 
 if __name__ == "__main__":
     wishMe()
-    takeCommand()
+    while True:
+        query = takeCommand().lower()
+        if 'wikipedia' in query:
+            query = query.replace('wikipedia', ' ',1)
+            query = wikipedia.summary(query, sentences=2)
+            print(f"Wikipedia result: {query}")
+            speak(query)
+        elif 'time' in query:
+            time = datetime.datetime.now().time().strftime('%H:%M')
+            speak('time is'+time)
+        elif 'open code' in query:
+            path = os.startfile('C:\\Program Files\\Microsoft VS Code\\Code.exe')
+            os.open(path)
+        elif 'open notepad' in query:
+            path = os.listdir("C:\WINDOWS\system32\notepad.exe")
+            os.startfile(path)
+        elif 'open youtube' in query:
+            webbrowser.open('www.youtube.com')
+        elif 'shutdown' in query:
+            exit(0)
